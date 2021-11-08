bdab03931e080c599cbff3b55a0c80c4a3927a44
juan <jcortesj@unal.edu.co>
2021-11-08 10:29:17 -0500

    esto es una prueba

---
+++ src/main/java/com/capaDatos/holamuundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.capaDatos;
+
+/**
+ *
+ * @author juan manuel
+ */
+public class holamuundo {
+    public void main (String[] args){
+        System.out.println("Hola mundo");
+    }
+}
