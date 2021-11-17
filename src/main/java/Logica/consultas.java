a663ca01853b22665da261a03f97077d7816beec
SimonRamos88 <79529391+SimonRamos88@users.noreply.github.com>
2021-11-16 20:23:15 -0600

    Update consultas.java

--- src/main/java/Logica/consultas.java
+++ src/main/java/Logica/consultas.java
@@ -9,8 +9,8 @@ import java.util.HashMap;
 
 public class consultas {
     
-    public  ArrayList<String> consultarNotas(Estudiante estudiante, Materia asignatura){
-        int id = estudiante.getID();
+    public ArrayList<String> consultarNotas(Estudiante estudiante, Materia asignatura){
+        String id = String.valueOf(estudiante.getID());
         ArrayList notaEstudiante = new ArrayList();
         ArrayList<Grupo> grupos = estudiante.getAtiende();
         int i = 0;
@@ -31,7 +31,7 @@ public class consultas {
         return notaEstudiante;  
     }
     public ArrayList<String> consultarNotas(Estudiante estudiante){
-        int id = estudiante.getID();
+        String id = String.valueOf(estudiante.getID());
         ArrayList notasEstudiante = new ArrayList();
         ArrayList<Grupo> grupos = estudiante.getAtiende();
         HashMap notasDelGrupo;
@@ -52,12 +52,79 @@ public class consultas {
         for(Object objeto:acudidos){
             Estudiante estudiante = (Estudiante) objeto;
             String cadena = estudiante.getNombre() + ": ";
-            cadena += consultarNotas(estudiante); 
+            cadena += String.join(" ", consultarNotas(estudiante)); 
             notasEstudiantes.add(cadena);
            
         } 
         
         return notasEstudiantes;
+    }
+    
+    public ArrayList<String> recorrerArregloHorario(ArrayList<Grupo> grupos){
+        ArrayList<String> horario = new ArrayList();
+        for(Object grupo : grupos){
+            Grupo grupoObjeto = (Grupo) grupo;
+            String nombreMateria = grupoObjeto.getAsignatura().getNombre();
+            String horarioMateria = String.join(", ", grupoObjeto.getHorario());
+            String calendario = nombreMateria + " " + horarioMateria;
+            horario.add(calendario);
+        }
+        return horario;
+    }
+    
+    public ArrayList<String> consultarHorario(Estudiante estudiante){
+        ArrayList<Grupo> grupos = estudiante.getAtiende();
+        ArrayList<String> horario = recorrerArregloHorario(grupos);
+        String cadena = "HORARIO ESTUDIANTE: ";
+        horario.add(0,cadena);      
+        return horario;
+        
+    }
+    
+    public ArrayList<String> consultarHorario(Profesor profe){
+        ArrayList<Grupo> grupos = profe.getCursos();
+        ArrayList<String> horario = recorrerArregloHorario(grupos);
+        String cadena = "HORARIO PROFE: ";
+        horario.add(0,cadena);      
+        return horario;       
+    }
+    
+    public ArrayList<String> consultarHorario(Papito acudiente){
+        ArrayList<Estudiante> acudidos = acudiente.getAcudeA();
+        ArrayList<String> horarioEstudiantes = new ArrayList();
+        for(Object objeto:acudidos){
+            Estudiante estudiante = (Estudiante) objeto;
+            String cadena = estudiante.getNombre() + ": ";
+            cadena += String.join(" ", consultarHorario(estudiante)); 
+            horarioEstudiantes.add(cadena);          
+        }    
+        return horarioEstudiantes;
+    }
+    
+    public ArrayList<Profesor> consultarProfes(Estudiante estudiante){
+        ArrayList<Profesor> profesores = new ArrayList();
+        ArrayList<Grupo> grupos = estudiante.getAtiende();
+        for(Object grupo : grupos){
+            Grupo grupoEstudiante = (Grupo) grupo;
+            Profesor profe = grupoEstudiante.getProfesor();
+            profesores.add(profe);
+        }    
+        return profesores;
+    }
+    
+    public ArrayList<ArrayList> consultarProfes(Papito acudiente){
+        ArrayList<Estudiante> acudidos = acudiente.getAcudeA();
+        ArrayList<ArrayList> profesYestudiantes = new ArrayList();
+        for(Object objeto:acudidos){            
+            Estudiante estudiante = (Estudiante) objeto;
+            ArrayList lista = new ArrayList();
+            String nombreEstudiante = estudiante.getNombre() + ": ";
+            lista.add(nombreEstudiante);
+            ArrayList<Profesor> profesores = consultarProfes(estudiante); 
+            lista.add(profesores);
+            profesYestudiantes.add(lista);
+        }    
+        return profesYestudiantes;
     }
     
     public  String consultarSubsidio(Estudiante consultor){
