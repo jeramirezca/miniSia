b6e48935ce8ed703c6327aabc830acc879f76834
juan manuel cortes <87251455+JcortesJ@users.noreply.github.com>
2021-11-16 21:42:33 -0500

    Update asignar.java
    añadi asignar nota y subsidio

--- src/main/java/Logica/asignar.java
+++ src/main/java/Logica/asignar.java
@@ -25,7 +25,7 @@ public class asignar {
     Subsidio tipoB = new Subsidio("B", 100.340, 1);
     Subsidio tipoC = new Subsidio("C", 320.340, 3);
 
-    public boolean asignarSubsidio(Estudiante solicitante, Subsidio tipo) {
+      public boolean asignarSubsidio(Estudiante solicitante, Subsidio tipo) {
 
         // Parte estudiante
         ArrayList<Subsidio> subsidiosDelEstudiante = solicitante.getSubsidios();
@@ -39,4 +39,59 @@ public class asignar {
         return true;
 
     }
+    
+    public ArrayList<Nota> asignarNota(Grupo grupo_asignar){
+       Scanner sc = new Scanner (System.in);
+       ArrayList comprobante_notas = new ArrayList<Nota>();
+        
+     HashMap  notas_Grupales = new HashMap <String, Object> ();
+        ArrayList<Estudiante> alumnos_por_Calificar = grupo_asignar.getEstudiantes();
+        
+        for (Estudiante alumno: alumnos_por_Calificar){
+            System.out.println("Introduzca Nota del estudiante: " + alumno.getNombre());
+            int calificacion = sc.nextInt();
+            Nota nota = new Nota(calificacion,alumno);
+            String nombre = alumno.getNombre();
+            notas_Grupales.put(nombre, nota);
+            comprobante_notas.add(nota);
+        }
+        
+        grupo_asignar.setNotasGrupo(notas_Grupales);
+        return comprobante_notas;
+    }
+    
+    //Para cambiar notas es necesario el documento del estudiante, y el grupo
+    public ArrayList<Nota> cambiarNota(Grupo grupo_asignar, int ID_solicitante) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList comprobante_notas = new ArrayList<Nota>();
+        Estudiante solicitante = new Estudiante();
+        HashMap notas_Grupales = grupo_asignar.getNotasGrupo();
+        ArrayList<Estudiante> alumnos_por_Calificar = grupo_asignar.getEstudiantes();
+        
+        //´primero hallamos el estudiante al cual le cambiaremos la nota, dentro de los estudiantes del grupo
+        for (Estudiante alumnos: alumnos_por_Calificar){
+            if(alumnos.getID()== ID_solicitante){
+                solicitante = alumnos;
+            }
+        }
+        
+        //despues si le cambiamos la nota. Para esto tenemos que eliminar la nota actual, y reemplazarla con la nueva
+
+        for (Estudiante alumno : alumnos_por_Calificar) {
+            if (alumno == solicitante) {
+                notas_Grupales.remove(alumno);
+                System.out.println("Cambiar la  Nota del estudiante: " + alumno.getNombre());
+                int calificacion = sc.nextInt();
+                Nota nota = new Nota(calificacion, alumno);
+                String nombre = alumno.getNombre();
+                notas_Grupales.put(nombre, nota);
+                comprobante_notas.add(nota);
+            }
+
+        }
+        
+        grupo_asignar.setNotasGrupo(notas_Grupales);
+        return comprobante_notas;
+    }
+    
 }
