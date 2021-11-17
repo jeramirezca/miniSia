28dd86b43a416765ef856d0b53756c537542ecd9
SimonRamos88 <79529391+SimonRamos88@users.noreply.github.com>
2021-11-16 20:24:25 -0600

    Update impresiones.java

--- src/main/java/UI/impresiones.java
+++ src/main/java/UI/impresiones.java
@@ -7,11 +7,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class impresiones {
-    public void mostrarNotas(ArrayList<String> notas){
-    for(String nota : notas){
-        System.out.print(nota + " ");
+    public void mostrarNotas(ArrayList<String> consultas){
+    for(String consulta : consultas){
+        System.out.print(consulta + " ");
+        }  
     }
     
+    public void mostrarProfes(ArrayList<Profesor> profes){
+        System.out.println("Los profesores del estudiante son: ");
+        for(Object profe : profes){
+        Profesor profesor = (Profesor) profe;
+        System.out.println(profesor.toString());
+        }
+    }
+    
+    public void mostrarProfesPapitos(ArrayList<ArrayList> profesYestudiantes){
+        for(int i=0; i<profesYestudiantes.size(); i++){
+            System.out.println(profesYestudiantes.get(i).get(0));
+            ArrayList<Profesor> profes = 
+                    (ArrayList<Profesor>) profesYestudiantes.get(i).get(1);
+            mostrarProfes(profes);
+
+        }
     }
     
 }
